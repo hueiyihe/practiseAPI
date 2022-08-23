9f2233729b8f27acc78d0d4141bd959f4971ef07
hueiyihe <phlpoi710482@gmail.com>
2022-08-23 16:02:54 +0800

    建立HttpClient  建立檢視表單頁面

---
+++ practiseMVC/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace practiseMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7037/api");
+        HttpClient client;
+        public CustomersController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+
+        public IActionResult Index()
+        {
+            List<Models.MVCCustomersModel> modelsList = new List<Models.MVCCustomersModel>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/Customers").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                modelsList = JsonConvert.DeserializeObject<List<Models.MVCCustomersModel>>(data);
+            }
+            return View(modelsList);
+        }
+    }
+}
