3c2806da1bfdc7f52406a3fe317d7458e6a18cb8
hueiyihe <phlpoi710482@gmail.com>
2022-08-23 15:01:36 +0800

    建立資料庫連線資訊 加入自訂的DbContext Service

--- practiseAPI/Program.cs
+++ practiseAPI/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var connString = builder.Configuration.GetConnectionString("nor");
+builder.Services.AddDbContext<practiseAPI.Models.NorthwindContext>(options =>
+    options.UseSqlServer(connString));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
